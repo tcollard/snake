48f20a2985f3222993f1a9add94501349bc6a177
tcollard <tcollard@student.42.fr>
2020-05-11 13:47:22 +0200

    fix: 🐛 (controller.dart) Change gesture detection

--- snake/lib/tools/controller.dart
+++ snake/lib/tools/controller.dart
@@ -1,63 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:snake/tools/infoGame.dart';
 import 'package:snake/screen.dart';
-import 'package:swipedetector/swipedetector.dart';
 
-class Controller extends StatefulWidget {
-  Controller({Key key}) : super(key: key);
-
-  @override
-  _ControllerState createState() => _ControllerState();
-}
-
-class _ControllerState extends State<Controller> {
+class Controller extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final InfoGameState gameInfo = InfoGame.of(context);
+    var startPosition;
+    var updatePosition;
 
-    return SwipeDetector(
-      onSwipeUp: () async {
-        if (gameInfo.info.move != 'Down') {
-          setState(() {
-            gameInfo.changeDirection('Up');
-          });
-        }
-      },
-      onSwipeDown: () async {
-        if (gameInfo.info.move != 'Up') {
-          setState(() {
-            gameInfo.changeDirection('Down');
-          });
-        }
-      },
-      onSwipeLeft: () async {
-        if (gameInfo.info.move != 'Right') {
-          setState(() {
-            gameInfo.changeDirection('Left');
-          });
-        }
-      },
-      onSwipeRight: () async {
-        if (gameInfo.info.move != 'Left') {
-          setState(() {
-            gameInfo.changeDirection('Right');
-          });
-        }
-      },
-      swipeConfiguration: SwipeConfiguration(
-        verticalSwipeMinDisplacement: 10.0,
-        verticalSwipeMinVelocity: 20,
-        horizontalSwipeMinDisplacement: 10.0,
-        horizontalSwipeMinVelocity: 20,
-      ),
-      child: GestureDetector(
-        onLongPressStart: (toto) {
-          gameInfo.updateAcceleration();
+    return GestureDetector(
+        onPanDown: (details) {
+          startPosition = details.globalPosition;
         },
-        onLongPressEnd: (toto) {
-          gameInfo.updateAcceleration();
+        onPanUpdate: (details) {
+          updatePosition = details.globalPosition;
         },
+        onPanEnd: (details) {
+          var direction = updatePosition - startPosition;
+          String setDirection = 'Up';
+          if (direction.dx != 0 && direction.dy != 0) {
+            if (direction.dx.abs() > direction.dy.abs()) {
+              if (direction.dx < 0 && gameInfo.info.move != 'Right') {
+                setDirection = 'Left';
+              } else if (gameInfo.info.move != 'Left'){
+                setDirection = 'Right';
+              }
+            } else {
+              if (direction.dy < 0  && gameInfo.info.move != 'Down') {
+                setDirection = 'Up';
+              } else if (gameInfo.info.move != 'Up'){
+                setDirection = 'Down';
+              }
+            }
+            print('SetDirection: $setDirection');
+            gameInfo.changeDirection(setDirection);
+          }
+        },
+        child: GestureDetector(
+            onLongPressStart: (nothing) {
+              gameInfo.updateAcceleration();
+            },
+            onLongPressEnd: (nothing) {
+              gameInfo.updateAcceleration();
+            },
         child: SnakeScreen()),
-    );
+        );
   }
 }
