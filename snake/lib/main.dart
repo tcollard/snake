862afeb53e27f638c2845b0879ce7d8a827405fe
tcollard <tcollard@student.42.fr>
2020-06-03 20:04:37 +0200

    feat: 🎸 (main.dart) Add splashScreeen Animation

--- snake/lib/main.dart
+++ snake/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:snake/game.dart';
+import 'package:flare_splash_screen/flare_splash_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  // runApp(MyApp());
+  runApp(MySplashScreen());
 }
+
 
 class MyApp extends StatelessWidget {
   @override
@@ -27,6 +30,25 @@ class SnakeGame extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Game(),
+    );
+  }
+}
+
+class MySplashScreen extends StatelessWidget {
+  const MySplashScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Snake Jazz',
+      home: SplashScreen.navigate(
+        name: './assets/animation/splashScreenAnimation.flr',
+        next: (_) => MyApp(),
+        until: () => Future.delayed(Duration(seconds: 0)),
+        startAnimation: 'splashScreen',
+        backgroundColor: Colors.white,
+      ),
     );
   }
 }
